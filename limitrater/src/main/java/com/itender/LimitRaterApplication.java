3312114869c42185f70a65093e297671f203c478
yuanhw <yuanheweiQAQ@163.com>
2022-05-10 16:33:51 +0800

    feat; java8新特性提交

---
+++ limitrater/src/main/java/com/itender/LimitRaterApplication.java
@@ -0,0 +1,16 @@
+package com.itender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author itender
+ * @date 2022/3/29 17:22
+ * @desc
+ */
+@SpringBootApplication
+public class LimitRaterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitRaterApplication.class, args);
+    }
+}
---
+++ limitrater/src/main/java/com/itender/limiter/redis/LimiterController.java
@@ -0,0 +1,41 @@
+package com.itender.limiter.redis;
+
+import com.google.common.collect.Lists;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author itender
+ */
+@RestController
+public class LimiterController {
+
+    private static final AtomicInteger ATOMIC_INTEGER_1 = new AtomicInteger();
+    private static final AtomicInteger ATOMIC_INTEGER_2 = new AtomicInteger();
+    private static final AtomicInteger ATOMIC_INTEGER_3 = new AtomicInteger();
+
+
+    @Limit(key = "limitTest", period = 10, count = 3)
+    @GetMapping("/limitTest1")
+    public int testLimiter1() {
+        return ATOMIC_INTEGER_1.incrementAndGet();
+    }
+
+    @Limit(key = "customer_limit_test", period = 10, count = 3, limitType = LimitType.CUSTOMER)
+    @GetMapping("/limitTest2")
+    public int testLimiter2() {
+
+        return ATOMIC_INTEGER_2.incrementAndGet();
+    }
+
+
+    @Limit(key = "ip_limit_test", period = 10, count = 3, limitType = LimitType.IP)
+    @GetMapping("/limitTest3")
+    public int testLimiter3() {
+
+        return ATOMIC_INTEGER_3.incrementAndGet();
+    }
+
+}
